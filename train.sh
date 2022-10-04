a252b203361eb70b8aabfd638e4da66e6ab8f8fc
Herman S <herman.sahota@gmail.com>
2022-10-04 17:37:33 +0000

    Modify train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config
 
-rm -rf experiments/reference/c*
+if [ -z "$1" ]
+then
+      echo "Nothing specified to run"
+else
+  echo running [${1}]
+  python experiments/model_main_tf2.py --model_dir=experiments/${1}/ --pipeline_config_path=experiments/${1}/pipeline_new.config
 
+  rm -rf experiments/${1}/c*
+fi
