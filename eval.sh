6300e7eaa4372cbf3a47da2a48faa3ead034e040
Herman S <herman.sahota@gmail.com>
2022-10-04 20:55:19 +0000

    Add eval script

---
+++ eval.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+if [ -z "$1" ]
+then
+      echo "Nothing specified to run"
+else
+  echo running [${1}]
+  python experiments/model_main_tf2.py --model_dir=experiments/${1}/ --pipeline_config_path=experiments/${1}/pipeline_new.config --checkpoint_dir=experiments/${1}/
+
+fi
+
